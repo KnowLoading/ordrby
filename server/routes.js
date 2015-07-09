4cf5d96afa026ce15f4e0733e15170fb4ea4d943
Alberto Perez Bermejo <alberto.perez.bermejo.contractor@bbva.com>
2015-07-09 09:30:09 +0200

    fix(server): fix routes

--- server/routes.js
+++ server/routes.js
@@ -16,8 +16,8 @@ module.exports = function(app) {
   app.use(basePath + '/auth', require('./auth'));
   
   // All undefined asset or api routes should return a 404
-  // app.route(basePath + '/:url(api|auth|components|app|bower_components|assets)/*')
-  //  .get(errors[404]);
+  app.route(basePath + '/:url(api|auth|components|app|bower_components|assets)/*')
+    .get(errors[404]);
 
   // All other routes should redirect to the index.html
   app.route(basePath + '/*')
